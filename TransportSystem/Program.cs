f89f25318c2a1538b5d4ee537df08bfa098c5107
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 16:19:41 +0500

    Models were used in the Program class

--- TransportSystem/Program.cs
+++ TransportSystem/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TransportSystem.Models;
+
+namespace TransportSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+
+        Console.Write("Enter your car's brand: ");
+        car.Brand = Console.ReadLine();
+
+        car.Move();
+        car.Drive();
+
+        Bike bike = new Bike();
+
+        Console.Write("Enter your bike's brand: ");
+        bike.Brand = Console.ReadLine();
+
+        bike.Move();
+        bike.Ride();
+
+        Train train = new Train();
+
+        Console.Write("Enter your train's brand: ");
+        train.Brand = Console.ReadLine();
+
+        train.Move();
+        train.CarryPassengers();
+    }
+}
