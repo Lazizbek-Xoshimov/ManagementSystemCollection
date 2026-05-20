2ce7a63a9ebeb9866ee191c16938787220286b99
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 16:37:40 +0500

    Created Teacher child class

---
+++ EmployeeSystem/Models/Teacher.cs
@@ -0,0 +1,10 @@
+namespace EmployeeSystem.Models;
+
+public class Teacher : Employee
+{
+    public void Teach()
+    {
+        Console.WriteLine($"{Name} is teaching.");
+        Console.WriteLine($"{Name}'s salary is {Salary}");
+    }
+}
