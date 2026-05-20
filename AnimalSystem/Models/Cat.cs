0749e6b8d8754f4cf8a1798477a70c177c1579eb
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 15:42:08 +0500

    Dog and Cat child classes created

---
+++ AnimalSystem/Models/Cat.cs
@@ -0,0 +1,9 @@
+namespace AnimalSystem.Models;
+
+public class Cat : Animal
+{
+    public void Meow()
+    {
+        Console.WriteLine($"{Name} is meowing.");
+    }
+}
---
+++ AnimalSystem/Models/Dog.cs
@@ -0,0 +1,9 @@
+namespace AnimalSystem.Models;
+
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine($"{Name} is barking.");
+    }
+}
