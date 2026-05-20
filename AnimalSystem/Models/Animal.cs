a24f0efdfd70ad151e6dc628c1f12a7d51e9338d
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 17:40:40 +0500

    The Encapsulation pinciple was applied to the Age property

--- AnimalSystem/Models/Animal.cs
+++ AnimalSystem/Models/Animal.cs
@@ -3,6 +3,7 @@ namespace AnimalSystem.Models;
 public class Animal
 {
     public string Name { get; set; }
+    protected int Age { get; set; }
 
     public void Eat()
     {
--- AnimalSystem/Models/Dog.cs
+++ AnimalSystem/Models/Dog.cs
@@ -6,4 +6,17 @@ public class Dog : Animal
     {
         Console.WriteLine($"{Name} is barking.");
     }
+
+    public void SetAge(int age)
+    {
+        if (age < 0)
+            Console.WriteLine("Age cannot be a negative value.");
+        else
+            Age = age;
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine($"{Name} is {Age} years old.");
+    }
 }
