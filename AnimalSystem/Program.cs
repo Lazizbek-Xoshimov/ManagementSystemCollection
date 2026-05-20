38f2f4c83e6b2f97206917d1ee497a87530a3d03
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 15:47:35 +0500

    Cat and Dog class methods used

--- AnimalSystem/Program.cs
+++ AnimalSystem/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using AnimalSystem.Models;
+
+namespace AnimalSystem;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        
+        Console.Write("Kuchugingizning ismini kiriting: ");
+        dog.Name = Console.ReadLine();
+
+        dog.Eat();
+        dog.Bark();
+
+        Cat cat = new Cat();
+
+        Console.Write("Mushugingizning ismini kiriting: ");
+        cat.Name = Console.ReadLine();
+
+        cat.Eat();
+        cat.Meow();
+    }
+}
