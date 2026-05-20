93380fd7fabd4f3e07f3cb5d7e18ce73f1e0340d
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 17:25:47 +0500

    Models were used in the Program class

--- UniversityManagement/Program.cs
+++ UniversityManagement/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using UniversityManagement.Models;
+
+namespace UniversityManagement;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student = new Student("Ali", 5);
+
+        Console.WriteLine(student.ShowProperties());
+
+        Teacher teacher = new Teacher("Hasan", "Math");
+
+        Console.WriteLine(teacher.ShowProperties());
+    }
+}
