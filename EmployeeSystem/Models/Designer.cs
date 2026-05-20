266fe5870faf2d23ed05de613c61675169ad8ad8
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 16:40:13 +0500

    Created Designer child class

---
+++ EmployeeSystem/Models/Designer.cs
@@ -0,0 +1,10 @@
+namespace EmployeeSystem.Models;
+
+public class Designer : Employee
+{
+    public void Design()
+    {
+        Console.WriteLine($"{Name} is designing.");
+        Console.WriteLine($"{Name}'s salary is {Salary}");
+    }
+}
