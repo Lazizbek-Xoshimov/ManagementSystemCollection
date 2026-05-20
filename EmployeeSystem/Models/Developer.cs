a6b85d990f5f68e8fb319b16e6a3ef4240ddbab0
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-20 16:35:09 +0500

    Created Developer child class

---
+++ EmployeeSystem/Models/Developer.cs
@@ -0,0 +1,10 @@
+namespace EmployeeSystem.Models;
+
+public class Developer : Employee
+{
+    public void Code()
+    {
+        Console.WriteLine($"{Name} is coding.");
+        Console.WriteLine($"{Name}'s salary is {Salary}");
+    }
+}
